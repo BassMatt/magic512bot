eb3ee6ea831af5c613941eaaaf50ba746be94f58
BassMatt <bassmatt0515@gmail.com>
2025-01-18 15:00:45 -0600

    add git

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+apk update && apk add --no-cache git
 DOCKER_USERNAME=bassmatt
 APPLICATION_NAME=magic512bot
 GIT_HASH=$(git log --format="%h" -n 1)
+
 
 docker build --tag ${DOCKER_USERNAME}/${APPLICATION_NAME}:${GIT_HASH} .
 
