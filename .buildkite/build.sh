b74ff7854025bf3827378a184f3c849ed8a85eb0
BassMatt <bassmatt0515@gmail.com>
2025-01-21 23:55:47 -0600

    trying build nubmer as pipeline

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
-apk update && apk add --no-cache git
 DOCKER_USERNAME=bassmatt
 APPLICATION_NAME=magic512bot
-GIT_HASH=$(git log --format="%h" -n 1)
 
 sleep 20 # sidecar takes a while to start up
 
-docker build --tag ${DOCKER_USERNAME}/${APPLICATION_NAME}:${GIT_HASH} --tag ${DOCKER_USERNAME}/${APPLICATION_NAME}:latest .
+docker build --tag ${DOCKER_USERNAME}/${APPLICATION_NAME}:${BUILDKITE_BUILD_NUMBER} --tag ${DOCKER_USERNAME}/${APPLICATION_NAME}:latest .
 
-docker push ${DOCKER_USERNAME}/${APPLICATION_NAME}:latest
+docker push ${DOCKER_USERNAME}/${APPLICATION_NAME}:${BUILDKITE_BUILD_NUMBER}
